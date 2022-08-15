256cfd300c429672c9dc410029bb04c0722bbf11
timothy timbol <ttimbol08@gmail.com>
2022-08-15 14:24:37 +0400

    Added db writing for user

--- backend/models/user.php
+++ backend/models/user.php
@@ -1,8 +1,21 @@
 <?php
+//object to return
+class UserRes
+{
+    public $Status;
+    public $Message;
 
+    function __construct($Status, $Message)
+    {
+        $this->Status = $Status;
+        $this->Message = $Message;
+    }
+}
+
+//user model
 class UserClass
 {
-    public $username;
+    private $username;
     private $password;
 
     function __construct($username, $password)
@@ -12,5 +25,38 @@ class UserClass
     }
 
     //find if use exist in databse
+    function findUser()
+    {
+        //get DB
+        $db = new SQLite3("../mydb.db");
+
+        //set query
+        $statement = $db->prepare("SELECT * FROM users WHERE Username = '$this->username'");
+        $result = $statement->execute();
+        $row = $result->fetchArray();
+
+        //return true or false if user exist in database.
+        if ($row) return true;
+
+        return false;
+    }
+
+    //create user
+    function createUser() {
+        //get db
+        $db = new SQLite3("../mydb.db");
+
+        //hash password then store to db
+        $hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+        $result =  $db->exec("INSERT INTO users(Username, Password) VALUES('$this->username', '$hash')");
+        
+        if ($result) {
+            echo json_encode(array("Status"=>201, "Message"=>"Sign up successful"));
+        } else {
+            echo "Error";
+        }
+        
+    }
 }
 ?>
